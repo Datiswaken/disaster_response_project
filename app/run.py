95e36863870abbbff1be7c336d207647063d757a
Jannis Lambinus <jannis.lambinus@real-digital.de>
2022-01-19 13:15:31 +0100

    Do small changes on website

--- app/run.py
+++ app/run.py
@@ -10,7 +10,7 @@ from typing import List
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from flask import Flask
-from flask import render_template, request, jsonify
+from flask import render_template, request
 from plotly.graph_objs import Bar, Scatter
 from sqlalchemy import create_engine
 
@@ -42,7 +42,6 @@ df = pd.read_sql_table('social_media_messages', engine)
 model = joblib.load("models/disaster_response_model.pkl")
 
 
-# index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
@@ -156,29 +155,25 @@ def index():
         }
     ]
 
-    # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
 
-    # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
 
-# web page that handles user query and displays model results
 @app.route('/go')
 def go():
-    # save user input in query
     query = request.args.get('query', '')
 
-    # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
+    number_categories = len([category for category in classification_results.values() if category == 1])
 
-    # This will render the go.html Please see that file.
     return render_template(
         'go.html',
         query=query,
-        classification_result=classification_results
+        classification_result=classification_results,
+        number_categories=number_categories
     )
 
 
